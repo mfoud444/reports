4ac3df3adf460e96f712b3fdbe9cfd767a2ea69f
Mohammed Foud <mfoud444@gmail.com>
2025-02-26 22:25:17 +0300

    Rename to index.php and disable error display

--- d.sh
+++ d.sh
@@ -1,3 +1,7 @@
-git add  .
-git commit -m "first commit"
-git push -u origin main
+mv server.php index.php
+sed -i 's/ini_set('\''display_errors'\'', 1);/ini_set('\''display_errors'\'', 0);/' index.php
+git add .
+git commit -m "Rename to index.php and disable error display"
+git push origin main
+mv index.php server.php
+sed -i 's/ini_set('\''display_errors'\'', 0);/ini_set('\''display_errors'\'', 1);/' server.php
--- index.php
+++ index.php
@@ -8,7 +8,7 @@
  */
 
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
+ini_set('display_errors', 0);
  
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? ''
